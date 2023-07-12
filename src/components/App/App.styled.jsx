c00e025c3c51cb3782d92bbaa3c7c1efa39a322d
Ruslan Tsuman <104848743+Ruslan-Ts@users.noreply.github.com>
2023-07-12 18:48:40 +0200

    +paket

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,11 +1,11 @@
-import styled from '@emotion/styled';
+import styled from 'styled-components';
 
 export const Container = styled.div`
-padding: 45px 15px;
+  padding: 45px 15px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center; 
-  
-background-color: orange;
+  align-items: center;
+
+  background-color: orange;
 `;
